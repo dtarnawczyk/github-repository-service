6ac34efae4a661cf7ec0666c0185127e4576e6a7
Dominik Tarnawczyk <dominikt@man.poznan.pl>
2017-04-26 11:04:01 +0200

    More configuration options on HttpClient

--- src/main/java/org/githubservice/config/RestClientConfiguration.java
+++ src/main/java/org/githubservice/config/RestClientConfiguration.java
@@ -1,5 +1,8 @@
 package org.githubservice.config;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +13,20 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestClientConfiguration {
 
-    @Value("${github.api.timeout}")
-    private int timeout;
+    @Value("${github.api.connectionTimeout}")
+    private int connectionTimeout;
+
+    @Value("${github.api.socketTimeout}")
+    private int socketTimeout;
+
+    @Value("${github.api.requestTimeout}")
+    private int requestTimeout;
+
+    @Value("${github.api.maxRedirects}")
+    private int maxRedirects;
+
+    @Value("${github.api.expectContinue}")
+    private boolean expectContinue;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -21,7 +36,21 @@ public class RestClientConfiguration {
     private ClientHttpRequestFactory getClientHttpRequestFactory() {
         HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
                 = new HttpComponentsClientHttpRequestFactory();
-        clientHttpRequestFactory.setConnectTimeout(timeout);
-        return clientHttpRequestFactory;
+        RequestConfig.Builder builder = RequestConfig.custom()
+                .setConnectTimeout(connectionTimeout)
+                .setSocketTimeout(socketTimeout)
+                .setConnectionRequestTimeout(requestTimeout)
+                .setMaxRedirects(maxRedirects)
+                .setExpectContinueEnabled(expectContinue);
+        if (maxRedirects <= 0) {
+            builder.setRedirectsEnabled(false);
+        }
+        CloseableHttpClient httpClient = HttpClientBuilder
+                .create()
+                .setDefaultRequestConfig(builder.build())
+                .disableCookieManagement()
+                .disableAuthCaching()
+                .build();
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 }
